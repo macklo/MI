36993ee4457c00f916917d8cc86a7195f56d47f0
Maciej Kłos <maciek2608@gmail.com>
2019-06-06 15:10:12 +0200

    Load data to a matrix

--- loadData.m
+++ loadData.m
@@ -2,4 +2,16 @@ close all
 clear
 clc
 
-A = readtable("data/dane10sec.xlsx");
+% A = readtable("data/dane10sec.xlsx");
+
+load('data/data.mat')
+
+podajniki = data(:, 17:22);
+
+R = corrcoef(podajniki);
+
+plot(podajniki)
+
+totalCorrelation = corrcoef(data);
+
+autoCorrelation = autocorr(data);
