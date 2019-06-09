10409a65c7583b0b98a5137c6fb4e32c40f09d7f
Marcin Skrzyczewski <skrzyczewski.marcin@gmail.com>
2019-06-09 22:48:29 +0200

    MLP & RN comparison

--- Untitled8.m
+++ Untitled8.m
@@ -1,52 +1,59 @@
-% plot(yMod(:,1))
-% hold on
-mlp=myNeuralNetworkFunction2(uMod);
-x1=uMod(6:end,:);
-xi1=uMod(1:5,:);
-rn=myNeuralNetworkFunction3(x1', xi1');
+u1=uDataSets{1};
+u2=uDataSets{2};
+u3=uDataSets{3};
+u4=uDataSets{4};
+y1=yDataSets{1};
+y2=yDataSets{2};
+y3=yDataSets{3};
+y4=yDataSets{4};
+TestedDataSet=3;
+
+ 
+if TestedDataSet==1
+    x1=u1(11:end,:);
+    xi1=u1(1:10,:);
+    mlpInput=u1;
+elseif TestedDataSet==2
+    x1=u2(11:end,:);
+    xi1=u2(1:10,:);
+    mlpInput=u2;
+elseif TestedDataSet==3
+    x1=u3(11:end,:);
+    xi1=u3(1:10,:);
+    mlpInput=u3;
+elseif TestedDataSet==4
+    x1=u4(11:end,:);
+    xi1=u4(1:10,:);
+    mlpInput=u4;
+end    
+mlp=mlp20(mlpInput);
+rn=lm10_10v2(x1', xi1');
 rn=rn';
-% plot(kkk(:,1))
-% figure 
-% plot(rn(:,1)-yMod(6:end,1),'.')
-% figure
-% plot(mlp(:,1)-yMod(:,1),'.')
-
-% eeee=normalize(rn(:,1),'range',[17.82-3,69.6-3]);
-% plot(eeee)
-% hold on
-% plot(data(6:end,23))
-% legend('model','dane')
-% diff=(data(6:end,23)-eeee(:,1))./data(6:end,23) *100
-% plot((eeee(:,1)-data(6:end,23)),'.')
-% err=mse(data(6:end,23),eeee)
-% eup=eeee;
-% 
-% for i=1:size(eup,1)
-%     if eup(i)>70
-%         eup(i)=70;
-%     end
-%             if eup(i)<17
-%         eup(i)=17;
-%             end
-% end
-% plot(diff)
-
-% lol=normalize(rn(:,2),'range',[24.23-6,75.24-6]);
-
 for i = 1:size(rn, 2)
-	column = rn(:, i);
     rn(:, i) = (((rn(:, i) + 1) / 2) * (maxVals(13+i) - minVals(13+i))) + minVals(13+i);
 end
+for i = 1:size(mlp, 2)
+    mlp(:, i) = (((mlp(:, i) + 1) / 2) * (maxVals(13+i) - minVals(13+i))) + minVals(13+i);
+end
 figure
-stairs(rn(1:2000,1))
+title('y1')
+stairs(rn(:,1))
 hold on
-stairs(data(6:2006,23))
-legend('model1','dane')
+stairs(mlp(11:end,1))
+hold on
+stairs(dataSets{TestedDataSet}(11:end,23))
+legend('model rn','model mlp','dane')
 
 figure
-stairs(rn(1:2000,2))
+title('y2')
+stairs(rn(:,2))
 hold on
-stairs(data(6:2006,24))
-legend('model1','dane')
+stairs(mlp(11:end,2))
+hold on
+stairs(dataSets{TestedDataSet}(11:end,24))
+legend('model rn','model mlp','dane')
+err(1)=immse(rn,dataSets{TestedDataSet}(11:end,23:24));
+err(2)=immse(mlp,dataSets{TestedDataSet}(:,23:24));
+err
 
 % plot(data(6:end,23)-lol)
